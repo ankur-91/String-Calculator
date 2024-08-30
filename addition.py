809691b749e70669df8bdd7c53d93c54ac4bceff
Ankur Gupta <ankurbpl91@gmail.com>
2024-08-30 15:32:42 +0530

    all the conditions executed

--- addition.py
+++ addition.py
@@ -1,24 +1,29 @@
 import re
 
+
+class NegativeNumberError(Exception):
+    def __init__(self, n):
+        super().__init__(f"Negative numbers not allowed: {n}")
+
 def add(li):
     res = 0
-    # negative_numbers = []
+    negative_numbers = []
 
-    for n in li:
-        # print(n)
-        # n = int(n)
-        res += int(n)
-    return res
+    try:
+        for n in li:
+            n = int(n)
+            if n < 0:
+                negative_numbers.append(n)
+                raise NegativeNumberError(n)
+            res += int(n)
+        return res
+    except NegativeNumberError as e:
+
+        print(e)
+
 
 def addition(st):
 
-    # # res_1 = 0
-    # split_st = st.split(',')
-    # # print(type(split_st))
-    # result_1 = add(split_st)
-    # print(result_1)
-
-    # second_split = re.split('\n|,', st)
     if st.startswith('\\'):
         cleaned_st = re.sub(r"^[^\d]*\n", "", st)
         cleaned_st = re.sub(r"^[^\d]*", "", cleaned_st)
@@ -26,16 +31,8 @@ def addition(st):
 
     second_split = re.split('\n|,', cleaned_st)
     print(second_split)
-    result_2 = add(second_split)
-    print(result_2)
-
-
-
-
-
-    # return res
-
-
+    result = add(second_split)
+    return result
 
 s = '\\;n*1,2,3'
 print(addition(s))
