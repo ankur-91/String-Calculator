c319b604dbe4bf64a4f0154624a9a2b1ab568fb1
Ankur Gupta <ankurbpl91@gmail.com>
2024-08-30 14:52:20 +0530

    second condition executed

--- addition.py
+++ addition.py
@@ -1,15 +1,29 @@
+import re
 
+
+def add(li):
+    res = 0
+    for n in li:
+        # print(n)
+        res += int(n)
+    return res
 
 def addition(st):
 
-    res = 0
-    split_st = st.split(',')
-    print(type(split_st))
-    for n in split_st:
-        # print(n)
-        res += int(n)
+    # # res_1 = 0
+    # split_st = st.split(',')
+    # # print(type(split_st))
+    # result_1 = add(split_st)
+    # print(result_1)
 
-    return res
+    second_split = re.split('\n|,', st)
+    print(second_split)
+    result_2 = add(second_split)
+    print(result_2)
+
+
+
+    # return res
 
 
 
