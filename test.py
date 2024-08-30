ab9e5e9775363f6b6f027511e0667342413a912a
Ankur Gupta <ankurbpl91@gmail.com>
2024-08-30 15:41:05 +0530

    final push

--- test.py
+++ test.py
@@ -1,12 +1,24 @@
 import unittest
-# from string_addition import addition
+from addition import NegativeNumberError, addition
+
 
 class StringAdditionTest(unittest.TestCase):
-    def test_addition(self, st):
-        res = addition(st)
-        self.assertEqual(st, 4)
 
+    def test_add_positive_numbers(self):
+        self.assertEqual(addition([4, 10, 12]), 26)
 
+    def test_add_negative_number(self):
+        with self.assertRaises(NegativeNumberError) as context:
+            addition([-1, -2, 3])
+        self.assertEqual(str(context.exception), "Negative numbers not allowed: -2")
+
+    def test_add_with_delimiter(self):
+        self.assertEqual(addition('\\;n*1,2,3'), 6)
+
+    def test_add_with_negative_number(self):
+        with self.assertRaises(NegativeNumberError) as context:
+            addition('\\;n*1,-2,3')
+        self.assertEqual(str(context.exception), "Negative numbers not allowed: -2")
 
 
 if __name__ == '__main__':
